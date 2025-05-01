7d822de9a6ee2f11aa83fcd452f47a9e8d6d813c
Amirahrizwan <95158682+Amirahrizwan@users.noreply.github.com>
2025-05-01 23:27:54 +0530

    Add files via upload

---
+++ map and array.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 4];
+let doubled = numbers.map(num => num * 2);
+console.log(doubled); // Output: [2, 4, 6, 8]
+
+
+let people = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 }
+  ];
+  let names = people.map(person => person.name);
+  console.log(names); // Output: ["Alice", "Bob", "Charlie"]
+  
