fad64d682e19a095e9e7261b12793b4412679ee9
Amirahrizwan <95158682+Amirahrizwan@users.noreply.github.com>
2025-05-01 23:24:26 +0530

    Add files via upload

---
+++ sort.js
@@ -0,0 +1,6 @@
+let str = "hello";
+let sorted = str.split('').sort().join('');
+console.log(sorted); // Output: "ehllo"
+
+let reversed = str.split('').reverse().join('');
+console.log(reversed); // Output: "olleh"
