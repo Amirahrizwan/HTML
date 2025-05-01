da0fb425490b5e4dcce0b6527cf89ce3d801e7df
Amirahrizwan <95158682+Amirahrizwan@users.noreply.github.com>
2025-05-01 23:16:33 +0530

    Add files via upload

---
+++ cond.js
@@ -0,0 +1,40 @@
+
+
+// if
+let age = 18;
+if (age >= 18) {
+  console.log("Adult");
+}
+
+
+// if else
+let score = 40;
+if (score >= 50) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
+
+
+// if else if
+let marks = 75;
+if (marks >= 90) {
+  console.log("A+");
+} else if (marks >= 75) {
+  console.log("A");
+} else {
+  console.log("B");
+}
+
+// switch
+let day = "Monday";
+switch (day) {
+  case "Monday":
+    console.log("Start of week");
+    break;
+  case "Friday":
+    console.log("Almost weekend");
+    break;
+  default:
+    console.log("Regular day");
+}
