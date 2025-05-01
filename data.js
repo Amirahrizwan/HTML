7707366a4acbce897d7b5190931db9fe21025946
Amirahrizwan <95158682+Amirahrizwan@users.noreply.github.com>
2025-05-01 23:09:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+let age = 25;              // Number
+let bigNum = 123n;         // BigInt
+let name = "John";         // String
+let isActive = false;      // Boolean
+let score;                 // Undefined
+let value = null;          // Null
+let id = Symbol("id");     // Symbol
+
+let user = { name: "John", age: 25 };   // Object
+let numbers = [1, 2, 3];                // Array
+function greet() { console.log("Hi"); } // Function
